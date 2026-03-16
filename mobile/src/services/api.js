88c68c448b6780e9e9fb2c6c8a449bfafa431028
JoseRCerdas17 <jose12roberto17@gmail.com>
2026-03-15 21:29:52 -0600

    feat: URL permanente de Railway en el mobile

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'https://grallatorial-rina-slimily.ngrok-free.dev';
+const API_URL = 'https://autocheck-app-production.up.railway.app';
 
 const api = axios.create({
   baseURL: API_URL,
